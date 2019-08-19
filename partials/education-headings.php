5eafc52704b0108fe7b753fb687153d738055f2d
domingobishop <domingobishop@gmail.com>
2019-08-19 16:23:23 +0100

    education headings template part

---
+++ partials/education-headings.php
@@ -0,0 +1,50 @@
+<div class="cards">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3">
+                <div class="card">
+                    <a href="#">
+                        <div class="entry-image" style="background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/img/);"></div>
+                        <div class="entry-content">
+                            <h3>Teachers</h3>
+                            <p>Resources and Workshop</p>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card">
+                    <a href="#">
+                        <div class="entry-image" style="background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/img/);"></div>
+                        <div class="entry-content">
+                            <h3>Students</h3>
+                            <p>Explore and Learn</p>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card">
+                    <a href="#">
+                        <div class="entry-image" style="background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/img/);"></div>
+                        <div class="entry-content">
+                            <h3>Outreach</h3>
+                            <p>Projects and Exhibitions</p>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card">
+                    <a href="#">
+                        <div class="entry-image" style="background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/img/);"></div>
+                        <div class="entry-content">
+                            <h3>Families</h3>
+                            <p>Events and Activities</p>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
