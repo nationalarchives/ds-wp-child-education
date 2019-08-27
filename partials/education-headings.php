b9b953626cdce5eb21fa96619613188e0d0cdab1
domingobishop <domingobishop@gmail.com>
2019-08-27 15:57:06 +0100

    create loop

--- partials/education-headings.php
+++ partials/education-headings.php
@@ -1,53 +1,48 @@
 <?php
 $theme_uri = make_path_relative_no_pre_path(get_stylesheet_directory_uri());
+$data = array (
+    array (
+        "title" => "Teachers",
+        "excerpt" => "Resources and Workshop",
+        "image" => $theme_uri."/img/",
+        "url" => ""
+    ),
+    array (
+        "title" => "Students",
+        "excerpt" => "Explore and Learn",
+        "image" => $theme_uri."/img/",
+        "url" => ""
+    ),
+    array (
+        "title" => "Outreach",
+        "excerpt" => "Projects and Exhibitions",
+        "image" => $theme_uri."/img/",
+        "url" => ""
+    ),
+    array (
+        "title" => "Families",
+        "excerpt" => "Events and Activities",
+        "image" => $theme_uri."/img/",
+        "url" => ""
+    )
+)
 ?>
 <div class="cards">
     <div class="container">
         <div class="row">
-            <div class="col-md-3">
-                <div class="card">
-                    <a href="#">
-                        <div class="entry-image" style="background-image: url(<?php echo $theme_uri; ?>/img/);"></div>
-                        <div class="entry-content">
-                            <h3>Teachers</h3>
-                            <p>Resources and Workshop</p>
-                        </div>
-                    </a>
+            <?php foreach ($data as $item) { ?>
+                <div class="col-md-3">
+                    <div class="card">
+                        <a href="<?php echo $item["url"]; ?>">
+                            <div class="entry-image" style="background-image: url(<?php echo $item["image"]; ?>);"></div>
+                            <div class="entry-content">
+                                <h3><?php echo $item["title"]; ?></h3>
+                                <p><?php echo $item["excerpt"]; ?></p>
+                            </div>
+                        </a>
+                    </div>
                 </div>
-            </div>
-            <div class="col-md-3">
-                <div class="card">
-                    <a href="#">
-                        <div class="entry-image" style="background-image: url(<?php echo $theme_uri; ?>/img/);"></div>
-                        <div class="entry-content">
-                            <h3>Students</h3>
-                            <p>Explore and Learn</p>
-                        </div>
-                    </a>
-                </div>
-            </div>
-            <div class="col-md-3">
-                <div class="card">
-                    <a href="#">
-                        <div class="entry-image" style="background-image: url(<?php echo $theme_uri; ?>/img/);"></div>
-                        <div class="entry-content">
-                            <h3>Outreach</h3>
-                            <p>Projects and Exhibitions</p>
-                        </div>
-                    </a>
-                </div>
-            </div>
-            <div class="col-md-3">
-                <div class="card">
-                    <a href="#">
-                        <div class="entry-image" style="background-image: url(<?php echo $theme_uri; ?>/img/);"></div>
-                        <div class="entry-content">
-                            <h3>Families</h3>
-                            <p>Events and Activities</p>
-                        </div>
-                    </a>
-                </div>
-            </div>
+            <?php } ?>
         </div>
     </div>
 </div>
