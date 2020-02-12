38dc1f93136b94efa64be1b767b3bc63f18ec118
JamesWChan <jwchan1993@gmail.com>
2020-02-12 10:38:06 +0000

    Add key stages partial

---
+++ partials/key-stages.php
@@ -0,0 +1,21 @@
+<div class="cards">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <h2>Key Stages</h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-12">
+                <p>Select sessions and resources by Key Stage:</p>
+                <ul id="disc-menu">
+                    <li><a href="/education/sessions-and-resources/?key-stage=ks1">1</a></li>
+                    <li><a href="/education/sessions-and-resources/?key-stage=ks2">2</a></li>
+                    <li><a href="/education/sessions-and-resources/?key-stage=ks3">3</a></li>
+                    <li><a href="/education/sessions-and-resources/?key-stage=ks4">4</a></li>
+                    <li><a href="/education/sessions-and-resources/?key-stage=ks5">5</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
