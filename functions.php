0e345375018ace8a921af3b9a56bbe2cc4b95f7c
JamesWChan <jwchan1993@gmail.com>
2020-09-24 15:39:12 +0100

    Add js to modify srcset and href attributes

--- functions.php
+++ functions.php
@@ -54,6 +54,10 @@ function tna_child_scripts()
     if (is_page_template('page-portal-landing.php')) {
         wp_register_script('jquery-3.5.1', get_stylesheet_directory_uri() . '/js/jquery-3.5.1.min.js', array(), '1.0');
         wp_enqueue_script( 'jquery-3.5.1' );
+    }
+    if(is_page_template('outreach/stories-resource.php')){
+        wp_register_script('jquery-3.5.1', get_stylesheet_directory_uri() . '/js/jquery-3.5.1.min.js', array(), '1.0');
+        wp_enqueue_script( 'jquery-3.5.1' );
         wp_register_script('stories-resource-script', get_stylesheet_directory_uri() . '/js/stories-resource-script.js', array(), '1.0');
         wp_enqueue_script( 'stories-resource-script' );
     }
--- js/stories-resource-script.js
+++ js/stories-resource-script.js
@@ -1,20 +1,10 @@
 $(document).ready(function(){
-	if($("#guidance").length){
-		try {
-			var $guidanceText = $("#guidance");
-			var ariaExpanded = $.parseJSON($("#guidance-btn").attr("aria-expanded").toLowerCase());
-			var ariaHidden = $.parseJSON($guidanceText.attr("aria-hidden").toLowerCase());
-
-			$("#guidance-btn").on('click', function(){
-				$guidanceText.toggle();
-				ariaHidden = !ariaHidden;
-				ariaExpanded = !ariaExpanded;
-				$guidanceText.attr('aria-hidden', ariaHidden);
-				$(this).attr('aria-expanded', ariaExpanded);
-			})
-		}
-		catch (e) {
-			return false;
-		}
-	}
+	var link = $('div#attachment_90 > a');
+	var img = $('div#attachment_90 > a > img.wp-image-90');
+	var href = link.attr('href');
+	var srcset = img.attr('srcset');
+	srcset = srcset.replaceAll('/education', '');
+	href = href.replace('/education', '');
+	img.attr('srcset', srcset);
+	link.attr('href', href)
 });
