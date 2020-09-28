e2b5e8833f45a2f59f0bada010ee38b8a679515d
JamesWChan <jwchan1993@gmail.com>
2020-09-28 15:48:48 +0100

    Replace src and wrap srcset in if condition

--- js/stories-resource-script.js
+++ js/stories-resource-script.js
@@ -2,9 +2,17 @@ $(document).ready(function(){
 	var link = $('a#five-photos-link');
 	var img = $('a#five-photos-link > img');
 	var href = link.attr('href');
+	var src = img.attr('src');
 	var srcset = img.attr('srcset');
-	srcset = srcset.replaceAll('/education', '');
+
+	src = src.replaceAll('/education', '');
 	href = href.replace('/education', '');
+
+	if(srcset){
+		srcset.replaceAll('/education', '');
+	}
+
+	img.attr('src', src);
 	img.attr('srcset', srcset);
-	link.attr('href', href)
+	link.attr('href', href);
 });
